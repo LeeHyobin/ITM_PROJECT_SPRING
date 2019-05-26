c5cda15236541068de1c9a3f708fb6ad07c41530
unknown <maladroit1@likelion.org>
2019-05-27 02:03:10 +0900

    update_basic_of_MVC

--- src/main/java/com/gsitm/meeting/branch/service/DepartmentHeadService.java
+++ src/main/java/com/gsitm/meeting/branch/service/DepartmentHeadService.java
@@ -1,13 +1,24 @@
 package com.gsitm.meeting.branch.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gsitm.meeting.branch.dao.DepartmentHeadDaoImpl;
+import com.gsitm.meeting.branch.dto.DepartmentHead;
 
 @Service
 public class DepartmentHeadService {
 
 	@Autowired
 	private DepartmentHeadDaoImpl deptHeadDao;
+	
+	public List<DepartmentHead> departmentHeadList(){
+		return deptHeadDao.departmentHeadList();
+	}
+	
+	public DepartmentHead departmentHeadOne(String empId) {
+		return deptHeadDao.departmentHeadOne(empId);
+	}
 }
